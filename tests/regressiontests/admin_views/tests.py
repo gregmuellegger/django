a021b7a67d93c160e5a8f9073eb874b645cf3b20
Gregor Müllegger <gregor@muellegger.de>
2011-12-11 12:50:21 +0100

    Fix for #17377

--- tests/regressiontests/admin_views/tests.py
+++ tests/regressiontests/admin_views/tests.py
@@ -2982,7 +2982,8 @@ class UserAdminTest(TestCase):
     def test_user_permission_performance(self):
         u = User.objects.all()[0]
 
-        with self.assertNumQueries(7):
+        ContentType.objects.clear_cache()
+        with self.assertNumQueries(8):
             response = self.client.get('/test_admin/admin/auth/user/%s/' % u.pk)
             self.assertEqual(response.status_code, 200)
 
